d72050415e6d344e644afe4ba9f87d63233b8cbe
Jinwoo Kim <kjinwoo12@gmail.com>
2023-07-21 18:47:40 +0900

    Init commit

---
+++ Source/LeagueOfWatch/LeagueOfWatchGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LeagueOfWatchGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LEAGUEOFWATCH_API ALeagueOfWatchGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
