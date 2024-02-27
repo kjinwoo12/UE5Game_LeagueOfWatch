2660db58f098ca376419cdff7fec1645a7591891
KimJinWoo <kjinwoo12@gmail.com>
2024-02-27 17:27:40 +0900

    Add weapon animations and assets
    bug! The pistol jog animation is not normal

---
+++ Source/LeagueOfWatch/Public/Equipment/LeagueEquipmentInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EquipmentInstance.h"
+#include "LeagueEquipmentInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LEAGUEOFWATCH_API ULeagueEquipmentInstance : public UEquipmentInstance
+{
+	GENERATED_BODY()
+	
+};
